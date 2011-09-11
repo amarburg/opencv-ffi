4f9e8ce22022527cd18998b65ef68015053d2ce8
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-09-12 09:56:52 +1200

    Moved the SURF functions into a sub-file in opencv-ffi/features2d/

---
+++ lib/opencv-ffi/features2d/surf.rb
@@ -0,0 +1,38 @@
+
+require 'opencv-ffi/cvffi'
+require 'opencv-ffi/core'
+require 'opencv-ffi/features2d/library'
+
+module CVFFI
+
+
+  class CvSURFParams < NiceFFI::Struct
+    layout :extended,      :int,
+           :upright,       :int,
+           :hessianThreshold, :double,
+           :nOctaves,      :int,
+           :nOctaveLayers, :int
+  end
+
+  class CvSURFPoint < NiceFFI::Struct
+    layout :pt, CvPoint2D32f,
+           :laplacian, :int,
+           :size, :int,
+           :dir,  :float,
+           :hessian, :float 
+  end
+
+  # CVAPI(void) cvExtractSURF( const CvArr* img, 
+  #                            const CvArr* mask,
+  #                            CvSeq** keypoints, 
+  #                            CvSeq** descriptors,
+  #                            CvMemStorage* storage, 
+  #                            CvSURFParams params, 
+  #                            int useProvidedKeyPts CV_DEFAULT(0)  );
+
+  attach_function :cvExtractSURF, [ :pointer, :pointer, 
+                                    :pointer, :pointer, 
+                                    :pointer, 
+                                    CvSURFParams.by_value, :cvBoolean ], :void
+
+end
