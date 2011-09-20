e6a46e00033563cf9c4f6752b48954f3338fffc6
Aaron Marburg <amarburg@notetofutureself.org>
2011-09-20 21:16:00 +1200

    Trying to force include paths

--- ext/opencv-ffi/mkrf_conf.rb
+++ ext/opencv-ffi/mkrf_conf.rb
@@ -2,6 +2,8 @@ require 'mkrf'
 
 Mkrf::Generator.new('libopencvffi', [ "*.cpp", "eigen/*.cpp", "fast/*.c" ]) { |g|
   g.include_library 'stdc++'
-  g.include_library 'opencv_core'
+  g.include_library 'opencv_core', '~/usr/lib'
+  g.include_header  'eigen3/signature_of_eigen3_matrix_library', '~/usr/include'
+  g.cflags += "-I~/usr/include "
 }
 
