69848032a7405061c4e341a6fbac404caebb0857
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-09-22 11:20:03 +1200

    Added a small note about specifying compiler in the Mkrf::Generator.new call

--- ext/opencv-ffi/mkrf_conf.rb
+++ ext/opencv-ffi/mkrf_conf.rb
@@ -1,5 +1,8 @@
 require '../mkrf-monkey'
 
+# The compiler for availability checking must be specified as 'g++'
+# otherwise it will use gcc and choke on Eigen
+#
 Mkrf::Generator.new('libopencvffi', [ "*.cpp", "eigen/*.cpp", "fast/*.c" ], { :compiler=>"g++"}) { |g|
   g.include_library 'stdc++'
   raise "Can't find 'opencv_core'" unless g.include_library 'opencv_core', 'main', "#{ENV['HOME']}/usr/lib"
