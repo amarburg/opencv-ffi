9a0567a1167c019b71f25eabe7745afd46a3ac0b
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-09-22 11:19:01 +1200

    Updated mkrf_conf.rb

--- ext/opencv-ffi/mkrf_conf.rb
+++ ext/opencv-ffi/mkrf_conf.rb
@@ -1,9 +1,9 @@
 require '../mkrf-monkey'
 
-Mkrf::Generator.new('libopencvffi', [ "*.cpp", "eigen/*.cpp", "fast/*.c" ]) { |g|
+Mkrf::Generator.new('libopencvffi', [ "*.cpp", "eigen/*.cpp", "fast/*.c" ], { :compiler=>"g++"}) { |g|
   g.include_library 'stdc++'
-  g.include_library 'opencv_core', ["#{ENV['HOME']}/usr/lib", "/usr/local/lib", "/usr/lib"]
-  g.include_header  'eigen3/signature_of_eigen3_matrix_library', "#{ENV['HOME']}/usr/include"
+  raise "Can't find 'opencv_core'" unless g.include_library 'opencv_core', 'main', "#{ENV['HOME']}/usr/lib"
+  g.include_header  'eigen3/Eigen/Core', "#{ENV['HOME']}/usr/include"
   g.cflags += "-I#{ENV['HOME']}/usr/include "
 }
 
