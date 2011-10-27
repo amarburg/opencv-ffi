3a8c318ab0f3dda361be671b3ed432f902fcc131
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-10-28 12:02:13 +1300

    Removed opencv-ffi-wrappers/surf from opencv-ffi-wrappers.rb, as it's handled by features2d.rb now

--- lib/opencv-ffi-wrappers.rb
+++ lib/opencv-ffi-wrappers.rb
@@ -1,6 +1,5 @@
 
 require 'opencv-ffi-wrappers/core'
-require 'opencv-ffi-wrappers/surf'
 require 'opencv-ffi-wrappers/matcher'
 require 'opencv-ffi-wrappers/misc'
 require 'opencv-ffi-wrappers/highgui'
