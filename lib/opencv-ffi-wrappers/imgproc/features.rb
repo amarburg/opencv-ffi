742e6842f1afab973b113604e28b13e18b23497a
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2011-10-28 12:15:51 +1300

    Split Params concept out into its own file in misc/

--- lib/opencv-ffi-wrappers/imgproc/features.rb
+++ lib/opencv-ffi-wrappers/imgproc/features.rb
@@ -1,35 +1,8 @@
 
 require 'opencv-ffi'
+require 'opencv-ffi-wrappers/misc/params'
 
 module CVFFI
-
-  class Params
-
-    def self.defaults; @defaults ||= {}; end
-
-    def self.param( *args )
-      while args.length > 0
-        default,name = [args.pop, args.pop]
-        puts "processing #{name} = #{default}"
-        defaults[ name ] = default
-      end
-    end
-
-    attr_reader :params
-
-    def initialize( opts = {} )
-      @params = {}
-      self.class.defaults.each_key { |k|
-        @params[k] = (opts[k] or opts[k.to_s] or self.class.defaults[k])
-        define_singleton_method( k ) { @params[k] }
-      }
-    end
-
-    def to_hash
-      @params
-    end
-  end
-
 
   class GoodFeaturesParams <  Params
 
---
+++ lib/opencv-ffi-wrappers/misc/params.rb
@@ -0,0 +1,33 @@
+
+require 'opencv-ffi'
+
+module CVFFI
+
+  class Params
+
+    def self.defaults; @defaults ||= {}; end
+
+    def self.param( *args )
+      while args.length > 0
+        default,name = [args.pop, args.pop]
+        defaults[ name ] = default
+      end
+    end
+
+    attr_reader :params
+
+    def initialize( opts = {} )
+      @params = {}
+      self.class.defaults.each_key { |k|
+        @params[k] = (opts[k] or opts[k.to_s] or self.class.defaults[k])
+        define_singleton_method( k ) { @params[k] }
+        instance_eval "def #{k}=(a); @params[#{k}] = a; end"
+      }
+    end
+
+    def to_hash
+      @params
+    end
+  end
+
+end
