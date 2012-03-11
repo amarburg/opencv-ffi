acdcab82b018bd1b267e2941b868d946e80ac073
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-03-12 10:21:43 +1300

    Added /usr/include/eigen3 to the include search path for the eigen extension.

--- ext/eigen/mkrf_conf.rb
+++ ext/eigen/mkrf_conf.rb
@@ -8,7 +8,9 @@ Mkrf::Generator.new('libcvffi_eigen', [ "*.cpp" ], { :compiler=>"g++"}) { |g|
   raise "Can't find 'opencv_core'" unless g.include_library 'opencv_core', 'main', "#{ENV['HOME']}/usr/lib"
   raise "Can't find #include<eigne3/Eigen/Core>" unless g.include_header  'eigen3/Eigen/Core', "#{ENV['HOME']}/usr/include"
 
-  # This is awkward.  Eigen's unsupported has internal includes <Eigen/Core>
-  g.cflags += "-I#{ENV['HOME']}/usr/include -I/usr/local/include/eigen3 -I#{ENV['HOME']}/usr/include/eigen3"
+  # This is awkward.  Eigen's unsupported directory references 
+  # includes like  <Eigen/Core> -- but /usr/include/eigen3 isn't
+  # in the default search path.
+  g.cflags += "-I#{ENV['HOME']}/usr/include -I/usr/include/eigen3 -I/usr/local/include/eigen3 -I#{ENV['HOME']}/usr/include/eigen3"
 }
 
