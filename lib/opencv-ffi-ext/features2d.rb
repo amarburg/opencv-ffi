0886d52c7ae3ce87a6b5ab3df557c300c06f3b2f
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-04-05 14:51:08 +1200

    Added subfiles to features2d.rb

--- lib/opencv-ffi-ext/features2d.rb
+++ lib/opencv-ffi-ext/features2d.rb
@@ -1,4 +1,5 @@
 
 require 'opencv-ffi-ext/features2d/sift'
 require 'opencv-ffi-ext/features2d/opensurf'
-
+require 'opencv-ffi-ext/features2d/harris_laplace'
+require 'opencv-ffi-ext/features2d/keypoint'
