61c0937ab17be905eba24ad3c8378802bf0f8005
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-04-17 11:54:51 +1200

    Added map_with_index! to map_with_index.

--- lib/opencv-ffi-wrappers/misc/map_with_index.rb
+++ lib/opencv-ffi-wrappers/misc/map_with_index.rb
@@ -5,6 +5,10 @@ module MapWithIndex
     each_with_index { |k,i| a << blk.yield( k,i ) }
     a
   end
+
+  def map_with_index!( &blk )
+    each_with_index { |k,i| self[i] = blk.yield( k,i ) }
+  end
 end
 
 
