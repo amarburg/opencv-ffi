8ed9a6c7e417732610ea683c4721c0485bc862ac
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2012-06-08 15:31:19 +1200

    Added unserialization to STAR keypoints.

--- lib/opencv-ffi-wrappers/features2d/star.rb
+++ lib/opencv-ffi-wrappers/features2d/star.rb
@@ -1,100 +1,149 @@
 require 'opencv-ffi/features2d'
+require 'opencv-ffi-wrappers/misc/params'
 require 'opencv-ffi-wrappers/core'
 require 'opencv-ffi-wrappers/vectors'
 
 module CVFFI
 
+  # Monkey patch some functions into CvStarKeypoint
+  class CvStarKeypoint
+
+    def x; pt.x; end
+    def y; pt.y; end
+
+    def self.keys
+      [:x, :y, :size, :response]
+    end
+
+    def keys; self.class.keys; end
+
+    def to_a
+      keys.map { |k| send k }
+    end
+
+    def self.from_a a
+      raise "Not enough elements in array to unserialize a STAR keypoint (#{a.length} != keys.length)" unless a.length == keys.length
+
+      # Slightly awkward because of the embedded CvPoint
+      kp = CvStarKeypoint.new
+      kp.pt.x = a.shift
+      kp.pt.y = a.shift
+      kp.size = a.shift
+      kp.response = a.shift
+      kp
+    end
+
+    def to_vector( homogeneous = true )
+      homogeneous ?  Vector.[]( x, y, 1 ) : Vector.[](x,y)
+    end
+    alias :to_Vector :to_vector
+
+    def to_Point
+      Point.new( x,y )
+    end
+
+    def to_CvPoint
+      CvPoint.new( x,y )
+    end
+
+    def ==(b)
+      keys.inject( true ) { |m,key|
+        m and ( (send key) == (b.send key) )
+      }
+    end
+
+  end
+
   module STAR
 
-    class Result 
-      attr_accessor :kp
-      def initialize( kp )
-        @kp = CVFFI::CvStarKeypoint.new(kp)
-      end
+    #    class Result 
+    #      attr_accessor :kp
+    #      def initialize( kp )
+    #        @kp = CVFFI::CvStarKeypoint.new(kp)
+    #      end
+    #
+    #      def pt; @kp.pt; end
+    #      def x;  pt.x; end
+    #      def y;  pt.y; end
+    #
+    #      def size; @kp.size; end
+    #      def response; @kp.response; end
+    #
+    #      def to_vector( homogeneous = true )
+    #        homogeneous ?  Vector.[]( x, y, 1 ) : Vector.[](x,y)
+    #      end
+    #      alias :to_Vector :to_vector
+    #      
+    #      def to_Point
+    #        pt.to_Point
+    #      end
+    #
+    #      def to_CvPoint
+    #        pt.to_CvPoint
+    #      end
+    #
+    #   end
 
-      def pt; @kp.pt; end
-      def x;  pt.x; end
-      def y;  pt.y; end
-
-      def size; @kp.size; end
-      def response; @kp.response; end
-
-      def to_vector( homogeneous = true )
-        homogeneous ?  Vector.[]( x, y, 1 ) : Vector.[](x,y)
-      end
-      alias :to_Vector :to_vector
-      
-      def to_Point
-        pt.to_Point
-      end
-
-      def to_CvPoint
-        pt.to_CvPoint
-      end
-
-
-   end
-
-    class ResultsArray
-      include Enumerable
-
-      attr_accessor :kp
-
-      def initialize( kp )
-        @kp = Sequence.new(kp)
-        @results = Array.new( @kp.length )
-      end
-
-      def result(i)
-        @results[i] ||= Result.new( @kp[i] )
-      end
-
-      def each
-        @results.each_index { |i| 
-          yield result(i) 
-        }
-      end
-
-      def [](i)
-        result(i)
-      end
-
-      def size
-        @kp.size
-      end
-      alias :length :size
-
+    class Results < SequenceArray
+      sequence_class  CvStarKeypoint
+      #
+      #      attr_accessor :kp
+      #
+      #      def initialize( kp )
+      #        @kp = Sequence.new(kp)
+      #        @results = Array.new( @kp.length )
+      #      end
+      #
+      #      def result(i)
+      #        @results[i] ||= Result.new( @kp[i] )
+      #      end
+      #
+      #      def each
+      #        @results.each_index { |i| 
+      #          yield result(i) 
+      #        }
+      #      end
+      #
+      #      def [](i)
+      #        result(i)
+      #      end
+      #
+      #      def size
+      #        @kp.size
+      #      end
+      #      alias :length :size
+      #
       def mark_on_image( img, opts )
         each { |r|
-          CVFFI::draw_circle( img, r.kp.pt, opts )
+          CVFFI::draw_circle( img, r.pt, opts )
         }
       end
     end
 
-    class Params
+    class Params  < CVFFI::Params
 
-      DEFAULTS = { maxSize: 45,
-                   responseThreshold: 30,
-                   lineThresholdProjected: 10,
-                   lineThresholdBinarized: 8,
-                   suppressNonmaxSize: 5 }
+      param :maxSize, 45
+      param :responseThreshold, 30
+      param :lineThresholdProjected, 10
+      param :lineThresholdBinarized, 8
+      param :suppressNonmaxSize, 5
 
-      def initialize( opts = {} )
-
-        @params = {}
-        DEFAULTS.each_key { |k|
-          @params[k] = (opts[k] or DEFAULTS[k])
-        }
-      end
+#      DEFAULTS = { maxSize: 45,
+#        responseThreshold: 30,
+#        lineThresholdProjected: 10,
+#        lineThresholdBinarized: 8,
+#        suppressNonmaxSize: 5 }
+#
+#      def initialize( opts = {} )
+#
+#        @params = {}
+#        DEFAULTS.each_key { |k|
+#          @params[k] = (opts[k] or DEFAULTS[k])
+#        }
+#      end
 
       def to_CvStarDetectorParams
-        par = @params
-        par.delete_if { |k,v| DEFAULTS.keys.include? k == false }
-        CVFFI::CvStarDetectorParams.new( par )
-      end
-
-      def to_hash
-        @params
+        CVFFI::CvStarDetectorParams.new( @params )
       end
     end
 
@@ -106,7 +155,7 @@ module CVFFI
       mem_storage = CVFFI::cvCreateMemStorage( 0 )
 
       keypoints = CVFFI::CvSeq.new CVFFI::cvGetStarKeypoints( img, mem_storage, params )
-      ResultsArray.new( keypoints )
+      Results.new( keypoints, mem_storage )
     end
   end
 end
--- test/wrappers/features2d/test_star.rb
+++ test/wrappers/features2d/test_star.rb
@@ -5,7 +5,19 @@ require 'test/setup'
 require 'opencv-ffi'
 require 'opencv-ffi-wrappers/features2d/star'
 
+module EachTwo
+  def each2(other, &blk)
+    raise "Can't call each2 unless arrays are same length (#{length} != #{other.length})" unless length == other.length
+
+    self.each_with_index { |mine,i|
+      blk.call( mine, other[i] )
+    }
+  end
+end
+
+
 class TestSTAR < Test::Unit::TestCase
+  include CVFFI
 
   def setup
     @img = TestSetup::test_image
@@ -22,7 +34,20 @@ class TestSTAR < Test::Unit::TestCase
     CVFFI::cvSaveImage( TestSetup::output_filename("starWrapperPts.jpg"), @img )
 
     ## Test some of the functions built into STAR::Result
+    puts "Star generated #{star.length} keypoints."
     p star[0]
- end
+
+    # Test serialization/unserialization
+    as_array = star.to_a
+    unserialized = STAR::Results.from_a( as_array )
+
+    assert_equal star.length, unserialized.length
+
+    star.extend EachTwo
+    star.each2(unserialized) { |kp,uns|
+      assert kp == uns, "Unserialized STAR feature #{uns} doesn't match original #{kp}"
+    }
+
+  end
 
 end
